cd1bbbb9d147d60e4768a8814579aea231d06240
ShivamNotfound <shivamshadow09@gmail.com>
2025-09-08 20:46:13 +0530

    Added symbols

--- newfile.py
+++ newfile.py
@@ -2,10 +2,11 @@
 
 num = int(input("Enter a number:"))
 
-print("List of values:")
 
 for i in range(num+1):
-    print(i, end=" ")
+    print(i, end=" * ")
+
+print("Those were the list of values!")
 
 
 print("Odd" if num%2!=0 else "Even")
